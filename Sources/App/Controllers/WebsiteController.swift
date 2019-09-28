7058e15b0d2e17bcf040fde776a975ac997419b3
Jiri Bucek <bucek.jiri@email.cz>
2019-09-28 18:24:48 +0200

    Leaf template

--- Sources/App/Controllers/WebsiteController.swift
+++ Sources/App/Controllers/WebsiteController.swift
@@ -11,14 +11,35 @@ import Vapor
 struct WebsiteController: RouteCollection {
     func boot(router: Router) throws {
         router.get(use: indexHandler)
+        router.get("acronyms", Acronym.parameter, use: acronymHandler)
     }
     
     func indexHandler(_ req: Request) throws -> Future<View> {
-        let context = IndexContent(title: "home")
-        return try req.view().render("index", context)
+        return Acronym.query(on: req).all().flatMap(to: View.self) { acronyms in
+            let context = IndexContent(title: "HomePage", acronyms: acronyms.isEmpty ? nil : acronyms)
+            return try req.view().render("index", context)
+        }
+    }
+    
+    func acronymHandler(_ req: Request) throws -> Future<View> {
+        return try req.parameters.next(Acronym.self).flatMap(to: View.self) { acronym in
+            return acronym.user.get(on: req).flatMap(to: View.self) { user in
+                let context = AcronymContext(title: acronym.long,
+                                             acronym: acronym,
+                                             user: user)
+                return try req.view().render("acronym", context)
+            }
+        }
     }
 }
 
-struct IndexContent: Encodable{
+struct IndexContent: Encodable {
     let title: String
+    let acronyms: [Acronym]?
+}
+
+struct AcronymContext: Encodable {
+    let title: String
+    let acronym: Acronym
+    let user: User
 }
